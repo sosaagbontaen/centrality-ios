45ca6ddd9fa30d40648072cf0abaf3711e3b468c
sosaagbontaen <sosaagbontaen@college.harvard.edu>
2022-07-25 09:36:02 -0700

    Fixed auto-layout and keyboard issues on modifyVC

--- CentralityiOS/CentralityiOS/ViewControllers/ModifyTaskModalViewController.m
+++ CentralityiOS/CentralityiOS/ViewControllers/ModifyTaskModalViewController.m
@@ -9,7 +9,7 @@
 #import "DateFormatHelper.h"
 #import "DateTools.h"
 
-@interface ModifyTaskModalViewController ()
+@interface ModifyTaskModalViewController () <UITextFieldDelegate, UITextViewDelegate>
 
 @end
 
@@ -32,6 +32,8 @@ static NSString * const kEditTaskMode = @"Editing";
     self.taskTitleInput.attributedText = toInput;
     
     [self.taskTitleInput addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    self.taskTitleInput.delegate = self;
+    self.taskDescInput.delegate = self;
 }
 
 
@@ -194,6 +196,65 @@ static NSString * const kEditTaskMode = @"Editing";
 
 - (IBAction)cancelAction:(id)sender {
     [self dismissViewControllerAnimated:YES completion:^{}];
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView
+{
+    return YES;
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    if([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        return NO;
+    }
+    return YES;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+
+
+#pragma mark - keyboard movements
+- (void)keyboardWillShow:(NSNotification *)notification
+{
+    CGSize keyboardSize = [[[notification userInfo] objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.view.frame;
+        f.origin.y = -keyboardSize.height;
+        self.view.frame = f;
+    }];
+}
+
+-(void)keyboardWillHide:(NSNotification *)notification
+{
+    [UIView animateWithDuration:0.3 animations:^{
+        CGRect f = self.view.frame;
+        f.origin.y = 0.0f;
+        self.view.frame = f;
+    }];
 }
 
 
