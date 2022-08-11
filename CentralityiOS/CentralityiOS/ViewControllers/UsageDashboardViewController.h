60e610eac549c8a44ddf71da0fc18381c9c44e01
sosaagbontaen <sosaagbontaen@college.harvard.edu>
2022-08-11 12:35:42 -0700

    Added dashboard and auto-layout

---
+++ CentralityiOS/CentralityiOS/ViewControllers/UsageDashboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  UsageDashboardViewController.h
+//  CentralityiOS
+//
+//  Created by Samuel Osa-Agbontaen on 8/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UsageDashboardViewController : UIViewController
+
+@end
+
---
+++ CentralityiOS/CentralityiOS/ViewControllers/UsageDashboardViewController.m
@@ -0,0 +1,21 @@
+//
+//  UsageDashboardViewController.m
+//  CentralityiOS
+//
+//  Created by Samuel Osa-Agbontaen on 8/11/22.
+//
+
+#import "UsageDashboardViewController.h"
+
+@interface UsageDashboardViewController ()
+
+@end
+
+@implementation UsageDashboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
